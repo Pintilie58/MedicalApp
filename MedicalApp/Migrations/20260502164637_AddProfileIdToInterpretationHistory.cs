0c9eddbef265c76ac5ad57829a02e3c0790521ea
Vasile Pintilie <vasilepintilie2003@gmail.com>
2026-05-02 19:49:23 +0300

    P1.3 migration - AddProfileIdToInterpretationHistory

---
+++ MedicalApp/Migrations/20260502164637_AddProfileIdToInterpretationHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileIdToInterpretationHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProfileId",
+                table: "InterpretationHistories",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileId",
+                table: "InterpretationHistories");
+        }
+    }
+}
