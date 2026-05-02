795440f987b5a05108de9e4d0d3f34c5b610ba85
Vasile Pintilie <vasilepintilie2003@gmail.com>
2026-05-03 00:40:31 +0300

    P1.4: Add RawJsonResult migration

---
+++ MedicalApp/Migrations/20260502211959_AddRawJsonResultToHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRawJsonResultToHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RawJsonResult",
+                table: "InterpretationHistories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RawJsonResult",
+                table: "InterpretationHistories");
+        }
+    }
+}
