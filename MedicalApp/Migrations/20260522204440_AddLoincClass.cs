fb1883ae018da818fbe30875fb171e4d02141e6d
Pintilie Vasile <vasilepintilie2003@gmail.com>
2026-05-23 19:09:33 +0300

    Add LoincClass column - EF migration

---
+++ MedicalApp/Migrations/20260522204440_AddLoincClass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoincClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "LoincDictionary",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "LoincDictionary");
+        }
+    }
+}
