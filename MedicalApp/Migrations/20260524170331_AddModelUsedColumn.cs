b1f844950c84b8a87ccd2f1e3927fa911c6b9d5e
Pintilie Vasile <vasilepintilie2003@gmail.com>
2026-05-24 20:12:01 +0300

    AddModelUsedColumn Flash vs Pro

---
+++ MedicalApp/Migrations/20260524170331_AddModelUsedColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MedicalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModelUsedColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ModelUsed",
+                table: "InterpretationHistories",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModelUsed",
+                table: "InterpretationHistories");
+        }
+    }
+}
